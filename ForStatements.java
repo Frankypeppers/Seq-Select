be86b6df47a14ca5d835675a3fc905d9d4f6bd8b
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-09-28 11:22:07 -0700

    for loops done

---
+++ ForStatements.java
@@ -0,0 +1,32 @@
+public class ForStatements
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("i is equal to "+i);
+        }
+        for (int j = 10; j > 0; j = j - 2) {
+            System.out.println("j is equal to "+j);
+        }
+        for (int k = 10; k == 0; k = k - 3) {
+            System.out.println( "k is equal to "+k);
+        }
+        /**
+         * K cannot equal 0 in this loop, 10 - 3 - 3 - 3 = 1
+         */
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        ForStatements fs = new ForStatements();
+        fs.run();
+    }
+}
+
+
